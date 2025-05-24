b03b770256c6b76f3db3d1cbe8979aac0846e877
Luis Rodriguez <lrodrigueza9719@gmail.com>
2025-05-23 23:36:13 -0400

    Modify frontend form to correctly send data to the backend

--- public/javascript/alta.js
+++ public/javascript/alta.js
@@ -1,31 +1,28 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  
   if (!validateForm(form)) {
-    return;
+    e.preventDefault();
+  } else {
+    return true;
   }
-
-  alert("Alta de Producto Exitosa");
 });
 
 const validateForm = (form) => {
   let valid = true;
   
   let product_name = form.querySelector(".product_name");
+  let product_description = form.querySelector(".product_description");
   let product_price = form.querySelector(".product_price");
-  let product_stock = form.querySelector(".product_stock");
-  let product_brand = form.querySelector(".product_brand");
-  let product_category = form.querySelector(".product_category");
-  let product_short_description = form.querySelector(".product_short_description");
-  let product_long_description = form.querySelector(".product_long_description");
-  let product_fee = form.querySelector(".product_fee");
-  let product_age_from = form.querySelector(".product_age_from");
-  let product_age_to = form.querySelector(".product_age_to");
+  let product_image_url = form.querySelector(".product_image_url");
 
   if (product_name.value === "") {
     giveError(product_name, "El nombre del artículo es requerido");
+    valid = false;
+  }
+
+  if (product_description.value === "") {
+    giveError(product_description, "La descripción del artículo es requerida");
     valid = false;
   }
 
@@ -34,45 +31,11 @@ const validateForm = (form) => {
     valid = false;
   }
 
-  if (product_stock.value === "") {
-    giveError(product_stock, "El stock del artículo es requerido");
+  if (product_image_url.value === "") {
+    giveError(product_image_url, "La URL de la imagen del artículo es requerida");
     valid = false;
   }
 
-  if (product_brand.value === "") {
-    giveError(product_brand, "La marca del artículo es requerida");
-    valid = false;
-  }
-
-  if (product_category.value === "") {
-    giveError(product_category, "La categoría del artículo es requerida");
-    valid = false;
-  }
-
-  if (product_short_description.value === "") {
-    giveError(product_short_description, "La descripción corta del artículo es requerida");
-    valid = false;
-  }
-
-  if (product_long_description.value === "") {
-    giveError(product_long_description, "La descripción larga del artículo es requerida");
-    valid = false;
-  }
-
-  if (product_fee.value === "") {
-    giveError(product_fee, "El cargo del artículo es requerido");
-    valid = false;
-  }
-
-  if (product_age_from.value === "") {
-    giveError(product_age_from, "La edad desde del artículo es requerida");
-    valid = false;
-  }
-
-  if (product_age_to.value === "") {
-    giveError(product_age_to, "La edad hasta del artículo es requerida");
-    valid = false;
-  }
 
   if (valid) {
     return true;
